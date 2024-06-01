7139de14152b8e620c1ae9050fcfaf455009568a
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-01 21:53:12 +0800

    Log in page Welcome messag created and google fonts changed

---
+++ chatapp_realtime_firebase/lib/pages/login_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 20),
+        child: Column(
+          children: <Widget>[_headerText()],
+        ),
+      ),
+    );
+  }
+
+  Widget _headerText() {
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Hi, Welcome Back!',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          Text(
+            "Hello again, you've been missed",
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w500,
+              color: Colors.grey,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ chatapp_realtime_firebase/lib/utils.dart
@@ -0,0 +1,6 @@
+import 'package:chatapp_realtime_firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+Future<void> setupFirebase() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
