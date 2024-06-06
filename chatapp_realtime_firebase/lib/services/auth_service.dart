934fbf57711b464be36f1e0e55ee46ee801c064f
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-06 15:11:54 +0800

    Auth Service Class Created

---
+++ chatapp_realtime_firebase/lib/services/auth_service.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  AuthService();
+  User? _user;
+  User? get user {
+    return _user;
+  }
+
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Future<bool> login(String email, String password) async {
+    try {
+      final UserCredential credential = await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      if (credential.user != null) {
+        _user = credential.user;
+        return true;
+      }
+    } catch (e) {
+      print(e);
+    }
+    return false;
+  }
+}
