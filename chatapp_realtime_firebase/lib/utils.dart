51dc90b2ccf88676a6b589f9a6bdbad16980eeaa
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-06 21:46:21 +0800

    Register Services using getIt done

--- chatapp_realtime_firebase/lib/utils.dart
+++ chatapp_realtime_firebase/lib/utils.dart
@@ -1,6 +1,13 @@
 import 'package:chatapp_realtime_firebase/firebase_options.dart';
+import 'package:chatapp_realtime_firebase/services/auth_service.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:get_it/get_it.dart';
 
 Future<void> setupFirebase() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
+
+Future<void> registerServices() async {
+  final GetIt getIt = GetIt.instance;
+  getIt.registerSingleton<AuthService>(AuthService());
 }
