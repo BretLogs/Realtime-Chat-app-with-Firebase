bcbebc1ab7b2eedc65de48f7697c281508a7b1c8
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-16 22:44:20 +0800

    Replaced dash_chat to dash_chat_2

--- chatapp_realtime_firebase/lib/pages/chat_page.dart
+++ chatapp_realtime_firebase/lib/pages/chat_page.dart
@@ -1,5 +1,8 @@
 import 'package:chatapp_realtime_firebase/models/user_profile.dart';
+import 'package:chatapp_realtime_firebase/services/auth_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:dash_chat_2/dash_chat_2.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key, required this.chatUser});
@@ -10,8 +13,39 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final GetIt _getIt = GetIt.instance;
+  late AuthService _authService;
+  ChatUser? currentUser, otherUser;
+  @override
+  void initState() {
+    super.initState();
+    _authService = _getIt.get<AuthService>();
+    currentUser = ChatUser(
+      id: _authService.user!.uid,
+      firstName: _authService.user!.displayName,
+    );
+    otherUser = ChatUser(
+      id: widget.chatUser.uid!,
+      firstName: widget.chatUser.name,
+      profileImage: widget.chatUser.pfpURL,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.chatUser.name!),
+      ),
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    return DashChat(
+      currentUser: currentUser!,
+      onSend: (ChatMessage message) {},
+      messages: const <ChatMessage>[],
+    );
   }
 }
