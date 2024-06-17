16237a0560638ad7044ec09e62004f9ec1decb97
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-17 19:40:55 +0800

    Chat App Done

--- chatapp_realtime_firebase/lib/pages/chat_page.dart
+++ chatapp_realtime_firebase/lib/pages/chat_page.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
+import 'package:chatapp_realtime_firebase/models/chat.dart';
 import 'package:chatapp_realtime_firebase/models/message.dart';
 import 'package:chatapp_realtime_firebase/models/user_profile.dart';
 import 'package:chatapp_realtime_firebase/services/auth_service.dart';
 import 'package:chatapp_realtime_firebase/services/database_service.dart';
+import 'package:chatapp_realtime_firebase/services/media_service.dart';
+import 'package:chatapp_realtime_firebase/services/storage_service.dart';
+import 'package:chatapp_realtime_firebase/utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
@@ -19,12 +25,18 @@ class _ChatPageState extends State<ChatPage> {
   final GetIt _getIt = GetIt.instance;
   late AuthService _authService;
   late DatabaseService _databaseService;
+  late MediaService _mediaService;
+  late StorageService _storageService;
+
   ChatUser? currentUser, otherUser;
   @override
   void initState() {
     super.initState();
     _authService = _getIt.get<AuthService>();
     _databaseService = _getIt.get<DatabaseService>();
+    _mediaService = _getIt.get<MediaService>();
+    _storageService = _getIt.get<StorageService>();
+
     currentUser = ChatUser(
       id: _authService.user!.uid,
       firstName: _authService.user!.displayName,
@@ -47,27 +59,105 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   Widget _buildUI() {
-    return DashChat(
-      currentUser: currentUser!,
-      onSend: _sendMessage,
-      messages: const <ChatMessage>[],
-      messageOptions: const MessageOptions(
-        showOtherUsersAvatar: true,
-        showTime: true,
-      ),
-      inputOptions: const InputOptions(
-        alwaysShowSend: true,
-      ),
-    );
+    return StreamBuilder(
+        stream: _databaseService.getChatData(currentUser!.id, otherUser!.id),
+        builder: (BuildContext context, AsyncSnapshot<DocumentSnapshot<Chat>> snapshot) {
+          Chat? chat = snapshot.data?.data();
+          List<ChatMessage> messages = <ChatMessage>[];
+          if (chat != null && chat.messages != null) {
+            messages = _generateChatMessagesList(chat.messages!);
+          }
+          return DashChat(
+            currentUser: currentUser!,
+            onSend: _sendMessage,
+            messages: messages,
+            messageOptions: const MessageOptions(
+              showOtherUsersAvatar: true,
+              showTime: true,
+            ),
+            inputOptions: InputOptions(
+              alwaysShowSend: true,
+              trailing: <Widget>[
+                _mediaMessageButton(),
+              ],
+            ),
+          );
+        });
   }
 
   Future<void> _sendMessage(ChatMessage chatMessage) async {
-    Message message = Message(
-      senderID: currentUser!.id,
-      content: chatMessage.text,
-      messageType: MessageType.Text,
-      sentAt: Timestamp.fromDate(chatMessage.createdAt),
-    );
-    await _databaseService.sendChatMessage(currentUser!.id, otherUser!.id, message);
+    if (chatMessage.medias?.isNotEmpty ?? false) {
+      if (chatMessage.medias!.first.type == MediaType.image) {
+        Message message = Message(
+          senderID: chatMessage.user.id,
+          content: chatMessage.medias!.first.url,
+          messageType: MessageType.Image,
+          sentAt: Timestamp.fromDate(chatMessage.createdAt),
+        );
+        await _databaseService.sendChatMessage(
+          currentUser!.id,
+          otherUser!.id,
+          message,
+        );
+      }
+    } else {
+      Message message = Message(
+        senderID: currentUser!.id,
+        content: chatMessage.text,
+        messageType: MessageType.Text,
+        sentAt: Timestamp.fromDate(chatMessage.createdAt),
+      );
+      await _databaseService.sendChatMessage(currentUser!.id, otherUser!.id, message);
+    }
+  }
+
+  List<ChatMessage> _generateChatMessagesList(List<Message> messages) {
+    List<ChatMessage> chatMessages = messages.map((Message m) {
+      if (m.messageType == MessageType.Image) {
+        return ChatMessage(
+          user: m.senderID == currentUser!.id ? currentUser! : otherUser!,
+          medias: <ChatMedia>[
+            ChatMedia(
+              url: m.content!,
+              fileName: '',
+              type: MediaType.image,
+            )
+          ],
+          createdAt: m.sentAt!.toDate(),
+        );
+      } else {
+        return ChatMessage(
+          user: m.senderID == currentUser!.id ? currentUser! : otherUser!,
+          text: m.content!,
+          createdAt: m.sentAt!.toDate(),
+        );
+      }
+    }).toList();
+    chatMessages.sort((ChatMessage a, ChatMessage b) {
+      return b.createdAt.compareTo(a.createdAt);
+    });
+    return chatMessages;
+  }
+
+  Widget _mediaMessageButton() {
+    return IconButton(
+        onPressed: () async {
+          File? file = await _mediaService.getImageFromGallery();
+          if (file != null) {
+            String chatID = generateChatID(uid1: currentUser!.id, uid2: otherUser!.id);
+            String? downloadURL = await _storageService.uploadImageToChat(file: file, chatID: chatID);
+            if (downloadURL != null) {
+              ChatMessage chatMessage = ChatMessage(
+                  user: currentUser!,
+                  createdAt: DateTime.now(),
+                  medias: <ChatMedia>[ChatMedia(url: downloadURL, fileName: '', type: MediaType.image)]);
+              _sendMessage(chatMessage);
+            }
+          }
+        },
+        icon: Icon(
+          Icons.image,
+          color: Theme.of(context).colorScheme.primary,
+        ));
   }
 }
