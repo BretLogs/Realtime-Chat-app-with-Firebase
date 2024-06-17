d60010d0e67371008f072d64eaaca8a08362bba3
Israel Breta <i_israel.breta@stratpoint.com>
2024-06-17 17:33:21 +0800

    _sendMessage created

--- chatapp_realtime_firebase/lib/pages/chat_page.dart
+++ chatapp_realtime_firebase/lib/pages/chat_page.dart
@@ -1,5 +1,7 @@
+import 'package:chatapp_realtime_firebase/models/message.dart';
 import 'package:chatapp_realtime_firebase/models/user_profile.dart';
 import 'package:chatapp_realtime_firebase/services/auth_service.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:dash_chat_2/dash_chat_2.dart';
@@ -44,8 +46,24 @@ class _ChatPageState extends State<ChatPage> {
   Widget _buildUI() {
     return DashChat(
       currentUser: currentUser!,
-      onSend: (ChatMessage message) {},
+      onSend: _sendMessage,
       messages: const <ChatMessage>[],
+      messageOptions: const MessageOptions(
+        showOtherUsersAvatar: true,
+        showTime: true,
+      ),
+      inputOptions: const InputOptions(
+        alwaysShowSend: true,
+      ),
+    );
+  }
+
+  Future<void> _sendMessage(ChatMessage chatMessage) async {
+    Message message = Message(
+      senderID: currentUser!.id,
+      content: chatMessage.text,
+      messageType: MessageType.Text,
+      sentAt: Timestamp.fromDate(chatMessage.createdAt),
     );
   }
 }
